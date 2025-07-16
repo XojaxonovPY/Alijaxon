bd7af70a8d6ef42de3215eb446a9aba2120f751a
User <asqarservis0000@gmail.com>
2025-07-16 13:14:31 +0300

    updated

--- apps/apps.py
+++ apps/apps.py
@@ -6,4 +6,19 @@ class AppsConfig(AppConfig):
     name = 'apps'
 
     def ready(self):
+        from apps.models import User
         import apps.signals
+        SUPER_PHONE = '9987'
+        SUPER_PASS = '1'
+
+        if SUPER_PHONE and SUPER_PASS:
+            if not User.objects.filter(phone_number=SUPER_PHONE).exists():
+                User.objects.create_superuser(
+                    phone_number=SUPER_PHONE,
+                    password=SUPER_PASS,
+                    is_staff=True,
+                    is_superuser=True
+                )
+                print(f"✅ Superuser created: {SUPER_PHONE}")
+            else:
+                print("ℹ️ Superuser already exists.")
